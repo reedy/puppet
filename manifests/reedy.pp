507de3dd2c3b3decc3d1aa4f3ca0d63a542bd983
Sam Reed <sam@reedyboy.net>
2014-04-21 17:39:30 +0100

    Update reedy.pp
    Moar

--- manifests/reedy.pp
+++ manifests/reedy.pp
@@ -1,13 +1,17 @@
 class reedy {
 	$username = "reedy"
 	$realname = "Sam Reed"
+	$uid = 512
 
 	user { $username:
 		name       => $username,
 		shell      => '/bin/bash',
+		managehome => true,
 		home       => "/home/$username",
 		ensure     => present,
 		allowdupe  => false,
+		uid        => $uid,
+		gid        => $gid,
 	}
 
 	ssh_authorized_key {
@@ -18,8 +22,12 @@ class reedy {
 			key     => "AAAAB3NzaC1yc2EAAAABIwAAAQEA3k6XjeMEmIHonzsmRBbHCkeVhxS6oObibs3PPP4DAO3WYXPIGBye+OpPtCpSZUuVp4t/GwnqIHCM0MrlVoFKeFcC3tHtVwmxhIsTp/RQRPjjKNdH60Iz6RlDTZ3TJDaYkYOiW7spdCONLzkYpOgkiph973aMNQ3D0vS87jht1apUl06bkxYeC+Bziq4DSBVNqpGKa+NqSYOvtS1kapwCYTtRm6YASb0YeMXzTUyfClgvq86h9XLsbx7klWgjHfKbfi/yheAm5EY6jxicnYaVAmy2gq2ERO9e2dVbpJihHmhPTpdRba5Eln0CoPkWrLVX0jyiAVB4biRtYoTtxGDPww=="
 	}
 	
+	group { $username:
+		gid => "$uid"
+	}
+	
 	group { 'sudo':
-            ensure    => 'present',
-            name      => 'sudo',
+		ensure    => 'present',
+		name      => 'sudo',
         }
 }
