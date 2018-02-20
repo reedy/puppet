9806c57335cde6b05b9f882d29f570932fd70dd5
Sam Reed <sam@reedyboy.net>
2018-02-20 17:46:00 +0000

    Second time lucky

--- modules/isreedyintheuk/manifests/init.pp
+++ modules/isreedyintheuk/manifests/init.pp
@@ -1,11 +1,11 @@
 class isreedyintheuk {
 	isreedyintheuk::site { 'isreedyintheuk.com': }
 	isreedyintheuk::site { 'isreedyinthe.uk':
-		$hostname => 'isreedyinthe.uk',
-		$default_server => '',
+		hostname => 'isreedyinthe.uk',
+		default_server => '',
 	}
 	isreedyintheuk::site { 'isreedyinthe.us':
-		$hostname => 'isreedyinthe.us',
-		$default_server => '',
+		hostname => 'isreedyinthe.us',
+		default_server => '',
 	}
 }
