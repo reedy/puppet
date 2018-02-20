7d19a8af42f3cbfec5880129a0a2551fd9c5506f
Sam Reed <sam@reedyboy.net>
2018-02-20 17:52:34 +0000

    Symlink to enabled

--- modules/isreedyintheuk/manifests/site.pp
+++ modules/isreedyintheuk/manifests/site.pp
@@ -3,8 +3,12 @@ define isreedyintheuk::site(
         $hostname = 'isreedyintheuk.com',
         $default_server = 'default_server',
 ) {
-        file { "/etc/nginx/sites-available/${hostname}.conf":
+	file { "/etc/nginx/sites-available/${hostname}.conf":
                 ensure  => file,
                 content => template('isreedyintheuk/site.conf.erb'),
+        }
+	file { "/etc/nginx/sites-enabled/${hostname}.conf":
+		ensure  => 'link',
+		target  => "/etc/nginx/sites-available/${hostname}.conf",
         }
 }
