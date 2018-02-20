cb58d073c7fc5ae8b63825dd741b1a3da491dde1
Sam Reed <sam@reedyboy.net>
2018-02-20 17:42:18 +0000

    Create three sites...

---
+++ modules/isreedyintheuk/manifests/init.pp
@@ -0,0 +1,11 @@
+class isreedyintheuk {
+	isreedyintheuk::site { 'isreedyintheuk.com': }
+	isreedyintheuk::site { 'isreedyinthe.uk':
+		$hostname = 'isreedyinthe.uk',
+		$default_server = '',
+	}
+	isreedyintheuk::site { 'isreedyinthe.us':
+		$hostname = 'isreedyinthe.us',
+		$default_server = '',
+	}
+}
